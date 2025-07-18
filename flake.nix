8ec3faf3f7145b09bc6e4bec92bd1373df480a4e
Familex <trener2990@gmail.com>
2025-07-19 02:06:11 +0300

    DEBUG: tweak build environment for local setup

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs = {
@@ -18,7 +17,7 @@
     packages = eachSystem (system: {
       default = self.packages.${system}.hypr-dynamic-cursors;
       hypr-dynamic-cursors = let
-        inherit (inputs.hyprland.packages.${system}) hyprland;
+        inherit (pkgsFor.${system}) hyprland;
         inherit (pkgsFor.${system}) stdenvNoCC gcc14;
 
         name = "hypr-dynamic-cursors";
@@ -46,6 +45,25 @@
             runHook postInstall
           '';
         };
+    });
+
+    devShells = eachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        name = "hypr-dynamic-cursors-shell";
+        nativeBuildInputs = with pkgs; [gnumake gcc14 clang-tools bear];
+        buildInputs = [pkgs.hyprland];
+        inputsFrom = [
+          pkgs.hyprland
+          self.packages.${system}.hypr-dynamic-cursors
+        ];
+        shellHook = ''
+          make clean
+          bear -- make -j$(nproc) ./out/dynamic-cursors.so
+          sed -i -e 's/c++23/c++2b/g' ./compile_commands.json
+        '';
+      };
     });
   };
 }
